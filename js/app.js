6dea80ec9e028b240366107313a3bafe8bd910f0
Dmytro Kulyk <13dimon1313@gmail.com>
2021-10-25 12:52:21 +0300

    stylize greeting section

--- js/app.js
+++ js/app.js
@@ -10,3 +10,44 @@ burgerBtnClose.addEventListener("click", (ev) => {
 window.addEventListener("load", (ev) => {
   burgerMenu.classList.add("transition");
 });
+// Video frame
+("use strict");
+function onYouTubePlayerAPIReady() {
+  var playerYoutube;
+
+  playerYoutube = new YT.Player("video-youtube__content", {
+    videoId: "Rs75vSex-jo",
+    playerVars: {
+      // 'controls': 0,
+      // 'showinfo': 0,
+      // 'disablekb': 1
+    },
+    events: {
+      onReady: onYouTubePlayerReady,
+    },
+  });
+}
+
+function onYouTubePlayerReady(event) {
+  // https://developers.google.com/youtube/iframe_api_reference#Events
+  var targetYoutubeVideo = event.target;
+  var videoDomElem = document.getElementById(
+    event.target.getIframe().getAttribute("id")
+  );
+  var newPlayBtn = videoDomElem.nextElementSibling;
+
+  newPlayBtn.addEventListener("click", function (event) {
+    targetYoutubeVideo.playVideo();
+    this.classList.add("hidden");
+    videoDomElem.classList.remove(
+      "video-youtube__content_hide-origin-play-btn"
+    );
+    videoDomElem.parentNode.classList.remove("video-youtube_overlay");
+  });
+}
+
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
