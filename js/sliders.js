3d2a1f1ec882ca702cae9450c46926d1fd72ec88
Dmytro Kulyk <13dimon1313@gmail.com>
2021-10-26 17:56:40 +0300

    create slider into partners section

---
+++ js/sliders.js
@@ -0,0 +1,12 @@
+//Sliders
+const swiper = new Swiper(".partners__slider", {
+  // Optional parameters
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 32,
+      grabCursor: true,
+    },
+  },
+});
