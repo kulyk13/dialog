1f8b953162ebeec75d265fd445149a5a8b05f72f
Dmytro Kulyk <13dimon1313@gmail.com>
2021-10-27 17:54:46 +0300

    initialize courses swiper slider

--- js/sliders.js
+++ js/sliders.js
@@ -1,4 +1,5 @@
 //Sliders
+//Partners slider
 const swiper = new Swiper(".partners__slider", {
   // Optional parameters
   breakpoints: {
@@ -6,6 +7,18 @@ const swiper = new Swiper(".partners__slider", {
       slidesPerView: 3,
       centeredSlides: true,
       spaceBetween: 32,
+      grabCursor: true,
+    },
+  },
+});
+
+//Courses slider
+const slider = new Swiper(".courses__slider", {
+  // Optional parameters
+  breakpoints: {
+    320: {
+      centeredSlides: true,
+      spaceBetween: 70,
       grabCursor: true,
     },
   },
