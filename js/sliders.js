c5729c9c547687c1e7b9354bf7609107d2d75ac0
Dmytro Kulyk <13dimon1313@gmail.com>
2021-10-28 16:25:23 +0300

    stylizing cours slider & pagination

--- js/sliders.js
+++ js/sliders.js
@@ -13,13 +13,44 @@ const swiper = new Swiper(".partners__slider", {
 });
 
 //Courses slider
+const slides = ["A1", "A2", "B1", "B2", "C1"];
+const values = [
+  0,
+  "начальный",
+  "элементарный",
+  "средний",
+  "выше среднего",
+  "продвинутый",
+];
 const slider = new Swiper(".courses__slider", {
   // Optional parameters
   breakpoints: {
     320: {
+      slidesPerView: 1,
       centeredSlides: true,
-      spaceBetween: 70,
+      spaceBetween: 100,
       grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          return (
+            '<li class="' +
+            className +
+            ' slider-pagination__item">' +
+            slides[index++] +
+            '<span class="slider-pagination__value"> &#160;-&#160;' +
+            values[index++] +
+            " уровень</span>" +
+            "</li>"
+          );
+        },
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+        dragSize: 266,
+      },
     },
   },
 });
